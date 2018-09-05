e034ebc9b3c472426cb7d3c882a58e8590a83e1c
vinay221097 <vinay221097@gmail.com>
2018-09-05 20:46:09 +0530

    Update settings.py

--- teleweb/settings.py
+++ teleweb/settings.py
@@ -122,7 +122,7 @@ STATIC_URL = '/static/'
 STATIC_DIRS=[
 os.path.join(BASE_DIR,"static"),
 ]
-STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 TG_SESSION_PATH = os.path.join(BASE_DIR,'sessions')
 MEDIA_URL = '/media/'
